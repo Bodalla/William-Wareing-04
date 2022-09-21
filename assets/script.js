7c0e96a4bcce63b094e7caf71c99825ce13d3158
William Wareing <wwareing11@gmail.com>
2022-09-21 16:48:45 -0700

    Added 'correct' and 'incorrect' alerts, made the potential answers into buttons, wrote out a few more questions and created a lot of js identifiers for later use, also added a lot more css values.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,57 @@
 const startButton = document.querySelector("#start-btn");
+
+//Question 1 id's
+    const q1c1 = document.querySelector("#q1c1");
+    const q1c2 = document.querySelector("#q1c2");
+    const q1c3 = document.querySelector("#q1c3");
+    const q1c4 = document.querySelector("#q1c4");
+
+//Question 2 id's
+    const q2c1 = document.querySelector("#q2c1");
+    const q2c2 = document.querySelector("#q2c2");
+    const q2c3 = document.querySelector("#q2c3");
+    const q2c4 = document.querySelector("#q2c4");
+
+//Question 3 id's
+    const q3c1 = document.querySelector("#q3c1");
+    const q3c2 = document.querySelector("#q3c2");
+    const q3c3 = document.querySelector("#q3c3");
+    const q3c4 = document.querySelector("#q3c4");
+
+//Question 4 id's
+    const q4c1 = document.querySelector("#q4c1");
+    const q4c2 = document.querySelector("#q4c2");
+    const q4c3 = document.querySelector("#q4c3");
+    const q4c4 = document.querySelector("#q4c4");
+
+//Question 5 id's
+    const q5c1 = document.querySelector("#q5c1");
+    const q5c2 = document.querySelector("#q5c2");
+    const q5c3 = document.querySelector("#q5c3");
+    const q5c4 = document.querySelector("#q5c4");
+
+//Question 6 id's
+    const q6c1 = document.querySelector("#q6c1");
+    const q6c2 = document.querySelector("#q6c2");
+    const q6c3 = document.querySelector("#q6c3");
+    const q6c4 = document.querySelector("#q6c4");
+
+//Question 7 id's
+    const q7c1 = document.querySelector("#q7c1");
+    const q7c2 = document.querySelector("#q7c2");
+    const q7c3 = document.querySelector("#q7c3");
+    const q7c4 = document.querySelector("#q7c4");
+
+//Question 8 id's
+    const q8c1 = document.querySelector("#q8c1");
+    const q8c2 = document.querySelector("#q8c2");
+    const q8c3 = document.querySelector("#q8c3");
+    const q8c4 = document.querySelector("#q8c4");
+
+//Link to 
+let incorrect = document.querySelector("#incorrect");
+let correct = document.querySelector("#correct");
+
 const quizQuestions = [
     "question-1",
     "question-2",
@@ -8,10 +61,9 @@ const quizQuestions = [
     "question-6",
     "question-7",
     "question-8",
-    //"question-9",
-    //"question-10",
-
 ];
+
+console.log(quizQuestions);
 
 //layout: question=q, choice=c
 const quizChoices = [
@@ -33,18 +85,42 @@ const quizChoices = [
 ],
 ];
 
+console.log(quizChoices);
 
+//let question1 = document.createElement (<ul>
+//    What is the starting index of an array?
+//</ul>);
+
+//let question2 =document.createElement 
 
 const startCard = document.querySelector(".card");
-const questionCard =document.querySelector(".question-card");
+const questionCard =document.querySelector(".question-card1");
 
-const questions = []
+//const questions = []
+
+function incorrectAlert() {
+    incorrect.style.display="block";
+    questionCard.style.display="none";
+};
+
+function correctAlert() {
+    correct.style.display="block";
+    questionCard.style.display="none";
+};
 
 function startQuiz() {
   startCard.style.display="none";
-  questionCard.style.display="block";  
+  questionCard.style.display="block";
+  //questionCard.style.color="blue";
+  //questionCard.style.fontsize="30pt";
 };
 
 startButton.addEventListener("click", startQuiz)
 
-const letters = document.createElement ("A","B","C", "D")
+q1c1.addEventListener("click", incorrectAlert)
+q1c2.addEventListener("click", incorrectAlert)
+q1c3.addEventListener("click", correctAlert)
+q1c4.addEventListener("click", incorrectAlert)
+
+
+//const letters = document.createElement ("A","B","C", "D")
